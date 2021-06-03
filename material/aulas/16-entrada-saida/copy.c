b891436e668f91df9640ef6e33ceaf1d1a87d5ed
thalials <thalials@al.insper.edu.br>
2021-06-03 17:11:24 -0300

    aula 16

---
+++ material/aulas/16-entrada-saida/copy.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main (int argc, char *argv[]) {
+    int source, dest;
+    char buf;
+
+    // somente leitua
+    source = open(argv[1], O_RDONLY);
+    // para escrita
+    dest = open(argv[2], O_WRONLY | O_CREAT, 0700);
+
+    int bytes_read = 1;
+
+    do {
+        bytes_read = read(source, &buf, 1);
+        if (bytes_read > 0) write(dest, &buf, 1);
+    } while (bytes_read != 0);
+
+    // fechar os arquivos
+    close(source);
+    close(dest);
+
+    return 0;
+}
