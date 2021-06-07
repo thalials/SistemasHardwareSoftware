a7849abb93b63534325e3967c56d48f9c2c64f9c
thalials <thalials@al.insper.edu.br>
2021-06-07 13:04:12 -0300

    exercicios de tad

--- material/aulas/11-tipos-de-dados/point2d.c
+++ material/aulas/11-tipos-de-dados/point2d.c
@@ -9,31 +9,44 @@ typedef struct _p Point2D;
 
 
 Point2D *point2D_new(double x, double y) {
-    return NULL;
+    Point2D *p = malloc(sizeof(Point2D));
+    p->x = x;
+    p->y = y;
+    return p;
 }
 
 
 void point2D_destroy(Point2D *p) {
-    
+    free(p);
 }
 
 double point2D_get_x(Point2D *p) {
-    return 0;
+    return p->x; 
 }
 
 double point2D_get_y(Point2D *p) {
-    return 0;
+    return p->y;
 }
 
 Point2D *point2D_add(Point2D *p1, Point2D *p2) {
-    return NULL;
+    Point2D *new_point = point2D_new(0, 0);
+    new_point->x = p1->x + p2->x;
+    new_point->y = p1->y + p2->y;
+
+    return new_point;
 }
 
 double point2D_theta(Point2D *p1, Point2D *p2) {
-    return 0;
+    double theta = (p1->y - p2->y)/(p1->x - p2->x);
+    return theta;
 }
 
 Point2D *point2D_scale(Point2D *p, double s) {
-    return NULL;
+    Point2D *scale = point2D_new(0,0);
+
+    scale->x = p->x * s;
+    scale->y = p->y * s;
+
+    return scale;
 }
 
